b600aeeb46830e6083951ab4722622123c7f32b7
Kevin Princiotta <kevinprinciotta@gmail.com>
2017-04-27 11:53:03 -0400

    Candify

--- candy.mk
+++ candy.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Candy stuff.
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := candy_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
